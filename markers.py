54444172f85c37da918572dc99451131b5671910
ned <n3d1117@users.noreply.github.com>
2021-12-19 16:22:37 +0100

    initial commit

---
+++ markers.py
@@ -0,0 +1,26 @@
+import json
+
+import pandas as pd
+
+
+def save_coordinates():
+    data = {'stations': []}
+
+    for name in ['SMART16', 'SMART24', 'SMART25', 'SMART26']:
+        df = pd.read_csv('data/smart/{}.csv'.format(name))
+        unique_coords_df = df[['lat', 'long']].drop_duplicates()
+
+        coords = []
+        for row in unique_coords_df.itertuples():
+            coords.append({
+                'lat': row.lat,
+                'long': row.long
+            })
+        data['stations'].append({'name': name, 'coords': coords})
+
+    with open('generated_data/locations.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    save_coordinates()
