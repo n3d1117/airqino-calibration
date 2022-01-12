3b9a53c39f16a733e2550bde9c68c85bf3a49129
ned <n3d1117@users.noreply.github.com>
2022-01-12 22:12:56 +0100

    temperature plots

--- temperature.py
+++ temperature.py
@@ -22,7 +22,8 @@ def plot_tair():
         fig.autofmt_xdate()
         plt.savefig('generated_data/plots/tair/{}.png'.format(filename))
 
-    do_plot(df, mdates.MonthLocator(interval=1), 'Temperature (TAIR) | All year', filename='tair')
+    do_plot(df.groupby(pd.Grouper(freq='3h')).mean().round(3), mdates.MonthLocator(interval=1),
+            'SMART16_new | Temperature | All year', filename='tair')
 
     date_range = pd.date_range('2020-09-01', '2021-08-31', freq='MS')
     for month in date_range:
@@ -31,66 +32,118 @@ def plot_tair():
         month_end = (month + MonthEnd(1)).strftime('%Y-%m-%d')
 
         month_dataset = df.loc[month_start: month_end]
-        do_plot(month_dataset, mdates.DayLocator(interval=3), 'Temperature (TAIR) | {}'.format(month_str),
+        do_plot(month_dataset, mdates.DayLocator(interval=3), 'SMART16_new | Temperature | {}'.format(month_str),
                 filename='tair_{}'.format(month_str.lower()))
 
 
-def plot_tair_scatterplot_and_compare():
+def plot_tair_scatterplot():
+    df = pd.read_csv('data/smart/{}.csv'.format('SMART16_new'))
+    df.set_index('data', inplace=True)
+    df.index = pd.to_datetime(df.index, utc=True)
+    df = df[['pm2_5', 'pm10', 'tair']]
+    df = df.groupby(pd.Grouper(freq='3h')).mean().round(3).dropna()
+
+    def seasons():
+        df_autumn = df[df.index.map(lambda t: t.month in [9, 10, 11] and t.year == 2020)]
+        df_winter = df[
+            df.index.map(lambda t: (t.month == 12 and t.year == 2020) or (t.month in [1, 2] and t.year == 2021))
+        ]
+        df_spring = df[df.index.map(lambda t: t.month in [3, 4, 5] and t.year == 2021)]
+        df_summer = df[df.index.map(lambda t: t.month in [6, 7, 8] and t.year == 2021)]
+        return df_autumn, df_winter, df_spring, df_summer
+
+    labels = ['Autumn', 'Winter', 'Spring', 'Summer']
+
+    # Scatterplot by season
+    for i, season in enumerate(seasons()):
+        # PM2.5
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_xlabel('Temperature (°C)')
+        ax.set_ylabel('PM2.5 (µg/m³)')
+        ax.set_title('SMART16_new_3h | TAIR vs PM2.5 | {}'.format(labels[i]))
+        X, y = season['tair'], season['pm2_5']
+        ax.scatter(X, y, marker='.')
+        linear_regressor = LinearRegression()
+        linear_regressor.fit(X.values.reshape((-1, 1)), y.values)
+        Y_pred = linear_regressor.predict(X.values.reshape((-1, 1)))
+        ax.plot(X, Y_pred, color='tab:red', linewidth=2, alpha=.4)
+        plt.savefig('generated_data/plots/tair/scatterplot_{}_pm2.5.png'.format(labels[i].lower()))
+
+        # PM10
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.set_xlabel('Temperature (°C)')
+        ax.set_ylabel('PM10 (µg/m³)')
+        ax.set_title('SMART16_new_3h | TAIR vs PM10 | {}'.format(labels[i]))
+        X, y = season['tair'], season['pm10']
+        ax.scatter(X, y, marker='.')
+        linear_regressor = LinearRegression()
+        linear_regressor.fit(X.values.reshape((-1, 1)), y.values)
+        Y_pred = linear_regressor.predict(X.values.reshape((-1, 1)))
+        ax.plot(X, Y_pred, color='tab:red', linewidth=2, alpha=.4)
+        plt.savefig('generated_data/plots/tair/scatterplot_{}_pm10.png'.format(labels[i].lower()))
+
+    # Scatterplot all year PM2.5
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i, season in enumerate(seasons()):
+        ax.scatter(season['tair'], season['pm2_5'], marker='.', label=labels[i], alpha=.5)
+    ax.legend(loc='upper right')
+    ax.set_xlabel('Temperature (°C)')
+    ax.set_ylabel('PM2.5 (µg/m³)')
+    ax.set_title('SMART16_new_3h | TAIR vs PM2.5')
+    plt.savefig('generated_data/plots/tair/scatterplot_pm2.5.png')
+
+    # Scatterplot all year PM10
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for i, season in enumerate(seasons()):
+        ax.scatter(season['tair'], season['pm10'], marker='.', label=labels[i], alpha=.5)
+    ax.legend(loc='upper right')
+    ax.set_xlabel('Temperature (°C)')
+    ax.set_ylabel('PM10 (µg/m³)')
+    ax.set_title('SMART16_new_3h | TAIR vs PM10')
+    plt.savefig('generated_data/plots/tair/scatterplot_pm10.png')
+
+
+def compare():
     df = pd.read_csv('data/smart/{}.csv'.format('SMART16_new'))
     df.set_index('data', inplace=True)
     df.index = pd.to_datetime(df.index, utc=True)
 
-    # Resample every 24h
-    df = df[['pm2_5', 'pm10', 'tair']]
-    grouped = df.groupby(pd.Grouper(freq='24h'))
-    df_resampled = grouped.mean().round(3)
-
-    arpat = pd.read_csv('generated_data/arpat/lu-{}.csv'.format('capannori_pm_dati_orari_cleaned_resampled_24h'))
-    arpat.set_index('data', inplace=True)
-    arpat.index = pd.to_datetime(arpat.index, utc=True)
-    arpat.rename(columns={'pm2.5': 'arpat_pm2.5', 'pm10': 'arpat_pm10'}, inplace=True)
-    arpat_pm = arpat[['arpat_pm2.5', 'arpat_pm10']]
-
-    merged = pd.merge(df_resampled, arpat_pm, on='data', how='inner').dropna()
-
-    df_autumn = merged[merged.index.map(lambda t: t.month in [9, 10, 11] and t.year == 2020)]
-    df_winter = merged[
-        merged.index.map(lambda t: (t.month == 12 and t.year == 2020) or (t.month in [1, 2] and t.year == 2021))]
-    df_spring = merged[merged.index.map(lambda t: t.month in [3, 4, 5] and t.year == 2021)]
-    df_summer = merged[merged.index.map(lambda t: t.month in [6, 7, 8] and t.year == 2021)]
-    labels = ['Autumn', 'Winter', 'Spring', 'Summer']
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    for i, season in enumerate([df_autumn, df_winter, df_spring, df_summer]):
-        ax.scatter(season['tair'], season['pm2_5'], marker='.', label=labels[i])
-        linear_regressor = LinearRegression()
-        X = season['tair'].values.reshape(-1, 1)
-        Y = season['pm2_5'].values.reshape(-1, 1)
-        linear_regressor.fit(X, Y)
-        Y_pred = linear_regressor.predict(X)
-        plt.plot(X, Y_pred)
-    ax.legend(loc='upper right')
-    ax.set_xlabel('Temperature (°C)')
-    ax.set_ylabel('PM2.5 (µg/m³)')
-    ax.set_title('SMART16 | TAIR vs PM2.5')
-    plt.savefig('generated_data/plots/tair/scatterplot.png')
-
     def compare_(df, start, end, period):
-        fig = plt.figure()
         month_dataset = df.loc[start: end]
+
+        # PM2.5
+        fig = plt.figure()
         ax = fig.add_subplot(111)
-        l1 = ax.plot(month_dataset['tair'], color='tab:red', label='temp (°C)', alpha=.7)
+        l1 = ax.plot(month_dataset['tair'], color='tab:red', label='Temp (°C)', alpha=.7)
         ax.tick_params(axis='y', labelcolor='tab:red')
         ax2 = ax.twinx()
-        l2 = ax2.plot(month_dataset['pm2_5'], color='tab:blue', label='pm2.5 (µg/m³)', alpha=.7)
+        l2 = ax2.plot(month_dataset['pm2_5'], color='tab:blue', label='PM2.5 (µg/m³)', alpha=.7)
         ax2.tick_params(axis='y', labelcolor='tab:blue')
         ax2.legend(l1 + l2, [l.get_label() for l in l1 + l2], loc='best')
         ax.xaxis.set_major_locator(mdates.HourLocator(interval=12))
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y %H:00'))
         fig.autofmt_xdate()
-        plt.title('SMART16 | TAIR vs PM2.5 | {}'.format(period))
+        plt.title('SMART16_new | TAIR vs PM2.5 | {}'.format(period))
         plt.savefig('generated_data/plots/tair/tair_pm2.5_compare_{}.png'.format(period.lower()))
+
+        # PM10
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        l1 = ax.plot(month_dataset['tair'], color='tab:red', label='Temp (°C)', alpha=.7)
+        ax.tick_params(axis='y', labelcolor='tab:red')
+        ax2 = ax.twinx()
+        l2 = ax2.plot(month_dataset['pm10'], color='tab:blue', label='PM10 (µg/m³)', alpha=.7)
+        ax2.tick_params(axis='y', labelcolor='tab:blue')
+        ax2.legend(l1 + l2, [l.get_label() for l in l1 + l2], loc='best')
+        ax.xaxis.set_major_locator(mdates.HourLocator(interval=12))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%y %H:00'))
+        fig.autofmt_xdate()
+        plt.title('SMART16_new | TAIR vs PM10 | {}'.format(period))
+        plt.savefig('generated_data/plots/tair/tair_pm10_compare_{}.png'.format(period.lower()))
 
     df = df[['pm2_5', 'pm10', 'tair']]
     grouped = df.groupby(pd.Grouper(freq='60Min'))
@@ -102,5 +155,6 @@ def plot_tair_scatterplot_and_compare():
 
 
 if __name__ == '__main__':
-    plot_tair_scatterplot_and_compare()
+    plot_tair_scatterplot()
+    compare()
     plot_tair()
